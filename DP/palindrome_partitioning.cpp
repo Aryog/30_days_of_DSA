99fe45938007aa3e0d55c71ae6f98259a6d30bfd
Aryog <yogesh000aryal@gmail.com>
2023-09-05 19:11:03 +0545

    day 12 completed

--- DP/palindrome_partitioning.cpp
+++ DP/palindrome_partitioning.cpp
@@ -1,3 +1,17 @@
+// Given a string s, partition s such that every
+// substring
+//  of the partition is a
+// palindrome
+// .
+
+// Return the minimum cuts needed for a palindrome partitioning of s.
+
+// Example 1:
+
+// Input: s = "aab"
+// Output: 1
+// Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+
 class Solution
 {
 public:
@@ -34,7 +48,24 @@ public:
         int mn = INT_MAX;
         for (int k = i; k <= j - 1; k++)
         {
-            int tMin = minPalinPartition(s, i, k) + minPalinPartition(s, k + 1, j) + 1;
+            int left;
+            int right;
+            if (t[i][k] != -1)
+                left = t[i][k];
+            else
+            {
+                left = minPalinPartition(s, i, k);
+                t[i][k] = left;
+            }
+
+            if (t[k + 1][j] != -1)
+                right = t[k + 1][j];
+            else
+            {
+                right = minPalinPartition(s, k + 1, j);
+                t[k + 1][j] = right;
+            }
+            int tMin = left + right + 1;
             mn = min(mn, tMin);
             t[i][j] = mn;
         }
